3e93d8be51cfa3decae56d5f26f12e93890daf15
kkly1995 <37456935+kkly1995@users.noreply.github.com>
2023-02-02 15:08:23 -0600

    Update quantum_moves.py

--- mcase/quantum_moves.py
+++ mcase/quantum_moves.py
@@ -5,11 +5,12 @@ def minimum_image(r, L):
     # but also possibly can be neglected (i hope...)
     return r - L * np.round(r / L)
 
-def spring_potential(r, rc, beta, tp):
-    # contribution to quantum spring potential from a SINGLE slice
-    val = np.sum(r**2 - 2 * r * rc)
-    return val / beta / tp
+def spring_potential(r, beta, tp):
+    val = r - np.roll(r, 1, axis=0)
+#    val = minimum_image(val, L)
+    return 0.5 * np.sum(val**2) / beta / tp
 
 def spring_force(r, rc, beta, tp):
-    # force from spring_potential(), on single slice
+    # force from spring_potential()
+#    return 2 * minimum_image(rc - r, L) / beta / tp
     return 2 * (rc - r) / beta / tp
